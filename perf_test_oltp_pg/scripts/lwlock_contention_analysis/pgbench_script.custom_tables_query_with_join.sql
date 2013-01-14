e118c281a113f4ba07444904d7ab19506bd5113c
Matt Smiley <matt.smiley@nwea.com>
2013-01-13 22:44:53 -0800

    Bug fix: typo in SQL script.

--- perf_test_oltp_pg/scripts/lwlock_contention_analysis/pgbench_script.custom_tables_query_with_join.sql
+++ perf_test_oltp_pg/scripts/lwlock_contention_analysis/pgbench_script.custom_tables_query_with_join.sql
@@ -1,2 +1,2 @@
 \setrandom val 1 10000
-SELECT a.key, a.val, b.val FROM pgbench_child a JOIN pgbench_parent b USING (key) WHERE val = :val;
+SELECT a.key, a.val, b.val FROM pgbench_child a JOIN pgbench_parent b USING (key) WHERE a.val = :val;
