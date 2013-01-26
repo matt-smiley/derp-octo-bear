689e2e00bdb64b926a532bab1799eaaa39a9a519
Matt Smiley <matt.smiley@nwea.com>
2013-01-26 10:02:18 -0800

    Added simple aggregator for semaphore capture output.  Useful for highlighting which capture files saw spinlock contention, assuming you run capture_semaphore_array_states.sh regularly to separate output files for each run.

---
+++ perf_test_oltp_pg/scripts/lwlock_contention_analysis/semaphore_filter.oneline_summary.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+awk '/^[0-9]/ && ( $3 > 0 || $4 > 0 ) { WAITERS++; NCOUNT+=$3; ZCOUNT+=$4 } END { printf "Total waiters: %d, Nonzero waiters: %d, Zero waiters: %d\n", WAITERS, NCOUNT, ZCOUNT; }'
