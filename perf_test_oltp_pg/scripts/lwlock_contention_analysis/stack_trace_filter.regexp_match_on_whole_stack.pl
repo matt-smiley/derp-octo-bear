3dbd03e660085a9b57a613011d44972112534526
Matt Smiley <matt.smiley@nwea.org>
2013-12-02 13:35:27 -0800

    Add support for an additional antipattern regexp and stack frame number reversal to stack_trace_filter.regexp_match_on_whole_stack.pl.  Backported from lldb_stack_trace_filter.regexp_match_on_whole_stack.pl.

--- perf_test_oltp_pg/scripts/lwlock_contention_analysis/stack_trace_filter.regexp_match_on_whole_stack.pl
+++ perf_test_oltp_pg/scripts/lwlock_contention_analysis/stack_trace_filter.regexp_match_on_whole_stack.pl
@@ -20,10 +20,23 @@ sub foreach_stack_do
     $func->(\@stack);
 }
 
+sub reverse_frame_numbering
+{
+    my ($stack) = @_;
+    my $frame_number = $#$stack;
+    foreach my $frame (@$stack) {
+        ($frame =~ s/Frame \d+/Frame $frame_number/) && $frame_number--;
+    }
+}
+
 my $pattern = shift @ARGV;
+my $antipattern = shift @ARGV;
+my $should_reverse_frame_numbering = (shift(@ARGV) || '') eq 'REVERSE';
 foreach_stack_do(
     sub ($) {
         my ($stack) = @_;
-        print join("", @$stack) . "\n" if join("", @$stack) =~ /$pattern/s;
+        reverse_frame_numbering($stack) if $should_reverse_frame_numbering;
+        my $whole_stack_str = join("", @$stack) . "\n";
+        print $whole_stack_str if ($whole_stack_str =~ /$pattern/s && (! $antipattern || $whole_stack_str !~ /$antipattern/s));
     }
 );
